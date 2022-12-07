b17ff3a91a767f1836c8bb4a75a0ef293a0df5bf
deeganp <deeganp.motro@gmail.com>
2022-12-07 16:10:49 -0500

    first commit

---
+++ Dest.js
@@ -0,0 +1,18 @@
+//Number 1 
+const obj = {
+numbers: {
+a: 1,
+b: 2
+}};
+
+
+const {a, b} = obj.numbers
+
+//Number 2 
+
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+//Number 3 
+
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
+
